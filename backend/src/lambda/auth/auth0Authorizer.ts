cb2faa81946fe3f564e316f1dc4a37db27b25fb1
Leonard Oduneye <leonardoduneye@outlook.com>
2023-01-12 14:42:10 +0100

    implemented auth function

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,10 +1,9 @@
-import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
+import { verify } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
@@ -55,38 +54,21 @@ export const handler = async (
 }
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
-  const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  try {
+    const token = getToken(authHeader)
+    // const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-
   const response = await Axios.get(jwksUrl)
-  
-  const keys = response.data.keys 
+  const responseData = response['data']['keys'][0]['x5c'][0]
+  const certificate = `-----BEGIN CERTIFICATE-----\n${responseData}\n-----END CERTIFICATE-----`
 
-  const signingKeys = keys.find(key => key.kid === jwt.header.kid)
-
-  logger.info('signingKeys', signingKeys)
-
-  if (!signingKeys) {
-
-    throw new Error('The JWKs endpoint did not contain any keys.')
+  return verify(token,certificate,{algorithms:['RS256']}) as JwtPayload
+  } catch (err) {
+    logger.error('Fail to authenticate', err)
   }
-
-  // get pem data
-  const pemData = signingKeys.x5c[0]
-
-  // convert pem data to cert
-  const cert = '-----BEGIN CERTIFICATE-----\n${pemData}\n-----END CERTIFICATE-----'
-
-  // verify token
-  const verifiedToken = verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
-
-  logger.info('verifiedToken', verifiedToken)
-  
-  return verifiedToken
 }
 
 function getToken(authHeader: string): string {
