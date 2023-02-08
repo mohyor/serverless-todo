5cd6873c25a24214f9d9448ccf3079c31566e9ae
Leonard Oduneye <leonardoduneye@outlook.com>
2023-02-08 09:14:49 +0100

    fixed request validation and updated the apiId

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '94uv4f71bh'
+const apiId = 'p3pq3nfose'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
